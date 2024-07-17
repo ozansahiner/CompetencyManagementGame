e0f38b56db7ef27d2771e391efd73ff260acae91
Zehrakturk <zehra.akturk15@gmail.com>
2024-07-17 16:46:55 +0300

    Doors animation added and edited map.

---
+++ GameDemo/Assets/DoorAnim/KapiScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kapi : MonoBehaviour
+{
+    public Animator animator; // Kapýyý açmak ve kapatmak için Animator bileþenini kullanacaðýz
+    private bool isOpen = true; // Kapýnýn açýk olup olmadýðýný kontrol etmek için
+    private Collider2D doorCollider; // Kapýnýn 2D Collider bileþeni
+
+    void Start()
+    {
+        // Animator ve Collider bileþenlerini al
+        animator = GetComponent<Animator>();
+        doorCollider = GetComponent<Collider2D>();
+
+        // Kapý kapalýyken Collider'ý etkinleþtir
+        doorCollider.enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isOpen)
+            {
+                OpenDoor();
+            }
+            else
+            {
+                CloseDoor();
+            }
+        }
+    }
+
+    void OpenDoor()
+    {
+        animator.SetBool("isOpen", true); // Animator'da "isOpen" parametresini true yaparak kapýyý aç
+        doorCollider.enabled = true; // Kapý açýldýðýnda Collider'ý devre dýþý býrak
+        isOpen = true;
+    }
+
+    void CloseDoor()
+    {
+        animator.SetBool("isOpen", false); // Animator'da "isOpen" parametresini false yaparak kapýyý kapat
+        doorCollider.enabled = false; // Kapý kapandýðýnda Collider'ý etkinleþtir
+        isOpen = false;
+    }
+}
